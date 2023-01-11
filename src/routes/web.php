5d672c3d8a46b5f7154ee21f4f42788e102c841f
navnit.amcodr <navnit.amcodr@gmail.com>
2023-01-11 16:00:24 +0530

    route change

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,4 @@
 use Sbash\Usermgmt\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('index', [UserController::class,'index']);
+Route::get('index', [UserController::class,'index'])->name('users')->middleware('web');
