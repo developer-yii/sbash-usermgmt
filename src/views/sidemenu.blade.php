bc7b50d215cfd9da6d845992436f10156ccf307e
navnit.amcodr <navnit.amcodr@gmail.com>
2023-01-11 16:22:43 +0530

    sidemenu add

---
+++ src/views/sidemenu.blade.php
@@ -0,0 +1,19 @@
+@section('sidemenu-item')
+<li class="nav-item">
+	<a href="#" class="nav-link">
+	  <i class="nav-icon fa fa-users"></i>
+	  <p>
+		User Management
+		<i class="fas fa-angle-left right"></i>                
+	  </p>
+	</a>
+	<ul class="nav nav-treeview">              
+	  <li class="nav-item">
+		<a href="javascript:void(0)" class="nav-link">
+		  <i class="fa fa-user nav-icon"></i>
+		  <p>Users</p>
+		</a>
+	  </li>                            
+	</ul>
+</li>
+@endsection
