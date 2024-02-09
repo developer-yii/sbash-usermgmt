08503878aa4f65c4c52344fd60e4af7b412b814e
navnit.amcodr <navnit.amcodr@gmail.com>
2024-02-09 15:01:47 +0530

    set password page theme changes

--- src/views/users/set_password.blade.php
+++ src/views/users/set_password.blade.php
@@ -1,63 +1,67 @@
 @extends('layouts.auth')
 
-@section('app-auth-css')
-<style type="text/css">  
-  .invalid-feedback{
-    display: block;    
-  }
-</style>
-@endsection
-
 @section('content')
-  <div class="login-logo">
-    <a href="{{ route('home')}} "><b>{{ config('app.name')}}</b></a>
-  </div>
+
+<div class="login-box">
   <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">{{ __('usermgmt')['user']['page']['set_password_msg'] }}</p>
+  <div class="">
+    <div class="card-header text-center">
+      <div class="col-12 mt-50 mb-50">
+        @if(config('app.project_alias') == 'sFlow')
+          <img class="mx-auto d-block" src="{{ asset('adminlte/dist/img/logo-login.png')}}" style="width:150px; align:center;">
+        @else
+          <img class="mx-auto d-block" src="{{ asset('img/sBash.png')}}" style="width:150px; align:center;">
+        @endif
+      </div>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">{{ __('usermgmt')['user']['page']['set_password_msg'] }}</p>                        
 
       <form action="{{ route('set-password.store') }}" method="post">
         @csrf
         <input type="hidden" name="user_id" value="{{$user->id}}">
-        <div class="form-group">          
-          <div class="input-group">            
-            <input type="password" name="password" class="form-control" placeholder="{{ __('usermgmt')['user']['form']['password'] }}">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          @error('password')
-            <span class="text-red text-red-500">{{ $message }}</span>
-          @enderror
-        </div>
-        <div class="form-group">          
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password_confirmation" placeholder="{{ __('usermgmt')['user']['form']['confirm_password'] }}">
+          <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('usermgmt')['user']['form']['password'] }}" required autofocus>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-        @error('password_confirmation')
-          <span class="text-red text-red-500">{{ $message }}</span>
-        @enderror
-      </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('usermgmt')['user']['form']['confirm_password'] }}" required autocomplete="current-password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+            @error('password_confirmation')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
         <div class="row">
+          
+          <!-- /.col -->
           <div class="col-12">
             <button type="submit" class="btn btn-primary btn-block">{{ __('usermgmt')['user']['buttons']['change_password']}}</button>
           </div>
           <!-- /.col -->
         </div>
-      </form>
-
+      </form>              
       <p class="mt-3 mb-1">
-        <a href="{{ route('login')}}">{{ __('usermgmt')['user']['buttons']['login']}}</a>
+        <a href="{{ route('login')}}" class="text-center">{{ __('usermgmt')['user']['buttons']['login']}}</a>
       </p>
     </div>
-    <!-- /.login-card-body -->
+    <!-- /.card-body -->
   </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
 @endsection
